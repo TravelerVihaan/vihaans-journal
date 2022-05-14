db15e4e2cefc5f0b217d6007d46240cf42da4b3f
ABCD ABCD <lukasz.biel@10g.pl>
2022-05-14 07:46:52 +0200

    Initial commit

---
+++ src/main/java/com/github/travelervihaan/vihaansjournal/VihaansJournalApplication.java
@@ -0,0 +1,13 @@
+package com.github.travelervihaan.vihaansjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VihaansJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VihaansJournalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/travelervihaan/vihaansjournal/VihaansJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.travelervihaan.vihaansjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VihaansJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
